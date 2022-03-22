56671f779922a4b9a5d5416a9a5c65245aec470d
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-03-22 16:47:31 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+import NotFound from '../components/NotFound';
+
+describe('testa o componente NotFound', () => {
+  it('testa se página contém um heading h2 com o texto "Page requested not found 😭"',
+    () => {
+      renderWithRouter(<NotFound />);
+
+      const notFoundTextEl = screen.getByRole(
+        'heading', { name: /Page requested not found/i, level: 2 },
+      );
+
+      expect(notFoundTextEl).toBeInTheDocument();
+    });
+
+  it('testa se página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"',
+    () => {
+      renderWithRouter(<NotFound />);
+
+      const imgNotFound = screen.getByRole(
+        'img', { name: 'Pikachu crying because the page requested was not found' },
+      );
+
+      expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
